980d6a35de137383ded375ac2669327316981020
TiagoSpada <tiago.spada.ts@gmail.com>
2024-05-07 10:59:37 +0200

    ciclo con un solo push

--- snack-4/main.js
+++ snack-4/main.js
@@ -2,30 +2,23 @@
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
 
-const arrayCorto = ["1", "2", "3", "4", "5", "6"];
-const arrayLungo = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const array1 = ["1", "2", "3", "4", "5", "6"];
+const array2 = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let arrayCorto;
+console.log(array1);
+console.log(array2);
 
-console.log(arrayCorto);
-console.log(arrayLungo);
 
-// controllo quele array è più corto
-if(arrayCorto.length > arrayLungo.length){
-    // ciclo che si ripete fino a quando un array è minore dell'altro
-    while(arrayLungo.length < arrayCorto.length){
-        // chiedo un elemento all'utente
-        const ArrayEl = prompt(`Aggiungi un elemento all'array 2`);
-        // aggiungo l'elemento all'array
-        arrayLungo.push(ArrayEl);
+    while(array2.length !== array1.length){
+        if(array1.length > array2.length){
+            arrayCorto = array2;
+        }else if (array2.length > array1.length) {
+            arrayCorto = array1;
+        }
+        const ElementoAggiunto = prompt(`Aggiungi un elemento all'array`);
+        arrayCorto.push(ElementoAggiunto)
     }
-}else if (arrayLungo.length > arrayCorto.length) {
-    while(arrayCorto.length < arrayLungo.length){
-        const ArrayEl = prompt("Aggiungi un elemento all'array 1");
-        arrayCorto.push(ArrayEl);
-    }
-} else{
-    alert("Gli array hanno già la stessa lunghezza")
-}
 
 console.log("Adesso gli array hanno la stessa lunghezza");
-console.log(arrayCorto);
-console.log(arrayLungo);
+console.log(array1 + " " + array1.length);
+console.log(array2 + " " + array2.length);
